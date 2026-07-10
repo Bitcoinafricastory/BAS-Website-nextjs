eeb744f1d23494b74074f00c9dcf17efac0346a9
Bitcoin Africa Story <bitcoinafricastory1@gmail.com>
2026-07-10 10:13:21 +0100

    Full news listing page rebuild: hero, filters, categorized grid, submit story form

---
+++ src/components/StoryEditor.jsx
@@ -0,0 +1,39 @@
+'use client';
+
+import dynamic from 'next/dynamic';
+import 'react-quill-new/dist/quill.snow.css';
+
+const ReactQuill = dynamic(() => import('react-quill-new'), { ssr: false });
+
+const fontSizeArr = ['10px', '12px', '14px', '16px', '18px', '20px', '24px', '30px', '36px', '48px'];
+
+const modules = {
+  toolbar: [
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    [{ font: [] }],
+    [{ size: fontSizeArr }],
+    [{ align: [] }],
+    ['bold', 'italic', 'underline', 'strike'],
+    [{ color: [] }, { background: [] }],
+    [{ script: 'sub' }, { script: 'super' }],
+    ['blockquote', 'code-block'],
+    [{ list: 'ordered' }, { list: 'bullet' }, { indent: '-1' }, { indent: '+1' }],
+    ['link', 'image', 'video'],
+    ['clean'],
+  ],
+};
+
+const formats = [
+  'header', 'font', 'size',
+  'bold', 'italic', 'underline', 'strike',
+  'color', 'background',
+  'script',
+  'blockquote', 'code-block',
+  'list', 'bullet', 'indent',
+  'align',
+  'link', 'image', 'video',
+];
+
+export default function StoryEditor({ value, onChange }) {
+  return <ReactQuill theme="snow" value={value} onChange={onChange} modules={modules} formats={formats} />;
+}
